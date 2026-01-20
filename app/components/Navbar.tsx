5c2b7d90c4030ba205008da6cde460ae6d875606
luffy <rutagandasalim@gmail.com>
2026-01-20 08:14:49 +0200

    update the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="dark fixed top-8 left-1/2 -translate-x-1/2 z-[99] w-[420px] bg-[#1a1a1a] backdrop-blur-lg rounded-[24px] overflow-hidden shadow-xl hidden md:block">
+    <header className="dark fixed top-8 left-1/2 -translate-x-1/2 z-[99] w-[400px] bg-[#1a1a1a] backdrop-blur-lg rounded-[24px] overflow-hidden shadow-xl hidden md:block">
       <div>
         <div className="flex items-center justify-between h-[52px] pl-4 pr-3">
           <div className="flex items-center gap-4">
@@ -9,7 +9,7 @@ export default function Navbar() {
             <span className="text-white text-sm hover:text-gray-300 transition-colors duration-200 cursor-pointer">Blog</span>
             <span className="text-white text-sm hover:text-gray-300 transition-colors duration-200 cursor-pointer">Docs</span>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
             <span className="text-white text-sm hover:text-gray-300 transition-colors duration-200 cursor-pointer">Login</span>
             <button className="bg-[#9580ff] hover:opacity-90 transition-colors duration-200 cursor-pointer text-white text-sm px-4 py-1.5 rounded-full font-medium">
               Register
